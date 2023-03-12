e664f4ba0077c2c8b175b8d12562ea78c4a97e49
Desale1 <dkdesale08@gmail.com>
2023-03-12 23:18:26 +0530

    create valid email pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public static void isValidFirstName(String firstName) {
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        String regex = "^[A-Z][a-z]{3,}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(firstName);
 
@@ -14,11 +14,21 @@ public class UserRegistration {
         else System.out.println("invalid");
 
     }
+
     public static void isValidName(String name) {
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        String regex = "^[A-Z][a-z]{3,}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(name);
 
+        if (m.matches()) System.out.println("valid");
+        else System.out.println("invalid");
+
+    }
+
+    public static void isValidEmail(String email) {
+        String regex = "^[a-z A-z 0-9]+(.[a-zA-Z0-9]+)?@[a-z]+[.][a-z]{2,3}+(.[a-zA-Z0-9]+)?$";
+        Pattern p = Pattern.compile(regex);
+       Matcher m = p.matcher(email);
         if (m.matches()) System.out.println("valid");
         else System.out.println("invalid");
 
